3df6d48bdb0ee5e10fe54f1cbe4b5c6abb995f79
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-02-15 22:29:43 +0400

    ui

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-let userDataT;
 var mypromise = new Promise(function(res,rej){
     var request = new XMLHttpRequest();
     request.open('get','https://randomuser.me/api/');
@@ -7,10 +6,11 @@ var mypromise = new Promise(function(res,rej){
     request.send();
 });
 mypromise.then(function(data){
-    userDataT = data.target.response;
+    userDataT = JSON.parse(data.target.response).results[0];
     console.log(userDataT);
+    requiredDATA = [];
+    
+
 }).catch(function(){
     console.log('error');
 });
-var UserData = '';
-console.log(UserData);
